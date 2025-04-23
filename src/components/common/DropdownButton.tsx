11b2122399a872231e2b6eee3d560797aaa9a1d3
김채림 <woqja0905@naver.com>
2025-04-23 11:08:43 +0900

    update : 필요없는 주석 삭제

--- src/components/common/DropdownButton.tsx
+++ src/components/common/DropdownButton.tsx
@@ -1,65 +1,3 @@
-// import { useState } from "react";
-// import styled from "styled-components";
-
-// const DropdownWrapper = styled.div`
-//   display: inline-block;
-//   position: relative;
-// `;
-
-// const DropdownButton = styled.button`
-//   padding: 8px 16px;
-//   background-color: #f0f0f0;
-//   cursor: pointer;
-//   width: 1408px;
-// `;
-
-// const DropdownMenu = styled.ul`
-//   position: absolute;
-//   top: 100%;
-//   left: 0;
-//   background-color: #f0f0f0;
-//   // border: 1px solid #ccc;
-//   list-style: none;
-//   padding: 8px;
-//   margin: 0;
-//   width: 100%;
-// `;
-
-// const DropdownItem = styled.li`
-//   padding: 8px;
-//   cursor: pointer;
-
-//   &:hover {
-//     background-color: #f0f0f0;
-//   }
-// `;
-
-// const Dropdown = ({ options }: { options: string[] }) => {
-//   const [isOpen, setIsOpen] = useState(false);
-
-//   const toggleDropdown = () => {
-//     setIsOpen(!isOpen);
-//   };
-
-//   return (
-//     <DropdownWrapper>
-//       {options.map((item, index) => (
-//         <DropdownButton key={index} onClick={toggleDropdown}>
-//           {item}
-//         </DropdownButton>
-//       ))}
-
-//       {isOpen && (
-//         <DropdownMenu>
-//           {options.map((item, index) => (
-//             <DropdownItem key={index}>{item}</DropdownItem>
-//           ))}
-//         </DropdownMenu>
-//       )}
-//     </DropdownWrapper>
-//   );
-// };
-
 import styled from "styled-components";
 // import { useState } from "react";
 import ArrowIcon from "../../assets/downArrow.svg";
@@ -84,7 +22,13 @@ const Dropdown = () => {
   return (
     <DropdownWrapper>
       <DropdownButton>
-        <div style={{ display: "flex", justifyContent: "flex-start" }}>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "flex-start",
+            width: "42px",
+          }}
+        >
           <p style={{ margin: "0px" }}>사업자</p>
         </div>
         <div style={{ display: "flex", justifyContent: "flex-end" }}>
