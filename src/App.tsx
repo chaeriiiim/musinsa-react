125a56ac5dd618412d7eae3bbbd7e7f0e1a0cee0
김채림 <woqja0905@naver.com>
2025-04-23 13:48:42 +0900

    update : main 파일 생성

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,16 @@ import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
 import Footer from "./components/common/Footer";
 import Header from "./components/common/Header";
+import Main from "./pages/Main/Main";
 import GlobalStyle from "./styles/GlobalStyle";
 import theme from "./styles/theme";
-
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
       <BrowserRouter>
         <Header />
+        <Main />
         <Footer />
       </BrowserRouter>
     </ThemeProvider>
---
+++ src/pages/Main/Main.tsx
@@ -0,0 +1,7 @@
+export default function Main() {
+  return (
+    <div
+      style={{ width: "100%", height: "200px", border: "1px solid red" }}
+    ></div>
+  );
+}
