b57d48aee744b72a3466fe8bb23c93b25ec98091
김채림 <woqja0905@naver.com>
2025-04-23 13:32:27 +0900

    update : footer 네이밍 수정

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -33,6 +33,33 @@ const FooterButton = styled.button`
   margin-right: 5px;
 `;
 
+const FooterCopy = styled.div`
+  padding: 0px 16px;
+
+  & p {
+    color: #666666;
+  }
+`;
+
+const FooterCopyTitle = styled.p`
+  color: #666666;
+  font-size: 13px;
+  margin: 10px 0px 0px 0px;
+`;
+
+const FooterCopyLink = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 8px;
+  height: 18px;
+  margin-top: 10px;
+
+  & a {
+    color: #666666;
+    font-size: 13px;
+  }
+`;
+
 const buttonList = [
   { label: "MUSINSA", path: "/" },
   { label: "BEAUTY", path: "/" },
@@ -53,38 +80,23 @@ export default function Footer() {
       </FooterButtonWrapper>
       <Dropdown />
 
-      <div
-        style={{
-          padding: "0px 16px",
-          // marginBottom: "5px",
-          marginTop: "10px",
-        }}
-      >
-        <p style={{ color: "#666666", fontSize: "13px", margin: "0px" }}>
-          @ MUSINSA ALL RIGHTS RESERVED
+      <FooterCopy>
+        <FooterCopyTitle>@ MUSINSA ALL RIGHTS RESERVED</FooterCopyTitle>
+
+        <FooterCopyLink>
+          <a href="#">개인정보처리방침</a>
+          <p>·</p>
+          <a href="#">이용약관</a>
+          <p>·</p>
+          <a href="#">결제대행 위탁사</a>
+        </FooterCopyLink>
+
+        <p style={{ color: "#8A8A8A", fontSize: "13px" }}>
+          일부 상품의 경우 주식회사 무신사는 통신판매의 당사자가 아닌
+          통신판매중개자로서 상품, 상품정보, 거래에 대한 책임이 제한될 수
+          있으므로, 각 상품 페이지에서 구체적인 내용을 확인하시기 바랍니다.
         </p>
-        <div
-          style={{
-            display: "flex",
-            alignItems: "center",
-            gap: "8px",
-            height: "18px",
-            marginTop: "10px",
-          }}
-        >
-          <a href="#" style={{ color: "#666666", fontSize: "13px" }}>
-            개인정보처리방침
-          </a>
-          <p style={{ color: "#666666" }}>·</p>
-          <a href="#" style={{ color: "#666666", fontSize: "13px" }}>
-            이용약관
-          </a>
-          <p style={{ color: "#666666" }}>·</p>
-          <a href="#" style={{ color: "#666666", fontSize: "13px" }}>
-            결제대행 위탁사
-          </a>
-        </div>
-      </div>
+      </FooterCopy>
     </FooterWrapper>
   );
 }
