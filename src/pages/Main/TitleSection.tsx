1e907e7493db294a9a03fc434f58fdba10950fe9
김채림 <woqja0905@naver.com>
2025-04-24 15:31:00 +0900

    update : title section 코드 추가

--- src/pages/Main/TitleSection.tsx
+++ src/pages/Main/TitleSection.tsx
@@ -1,12 +1,34 @@
+import styled from "styled-components";
+
+const TitleWrapper = styled.div`
+  width: 100%;
+  height: 72px;
+  padding: 12px 16px;
+  background-color: white;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
 export default function TitleSection() {
   return (
-    <div
-      style={{
-        width: "100%",
-        height: "62px",
-        padding: "12px 16px",
-        backgroundColor: "white",
-      }}
-    ></div>
+    <TitleWrapper>
+      <p>
+        스포츠 종목 아이템 추천
+        <br />
+        등산/아웃도어
+      </p>
+
+      <a
+        href="#"
+        style={{
+          fontSize: "13px",
+          color: "#666666",
+          textDecorationLine: "underline",
+        }}
+      >
+        더보기
+      </a>
+    </TitleWrapper>
   );
 }
