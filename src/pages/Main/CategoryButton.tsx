2a8f8b00362328d94dbefce42c54f0f04e6be6c9
김채림 <woqja0905@naver.com>
2025-04-24 18:13:13 +0900

    fix : category button 수정

--- src/pages/Main/CategoryButton.tsx
+++ src/pages/Main/CategoryButton.tsx
@@ -1,12 +1,12 @@
+import styled from "styled-components";
+
+const CategoryWrapper = styled.div`
+  width: 100%;
+  height: 116px;
+  padding: 16px;
+  background-color: #f5f5f5;
+`;
+
 export default function CategoryButton() {
-  return (
-    <div
-      style={{
-        width: "100%",
-        height: "116px",
-        padding: "16px",
-        backgroundColor: "#F5F5F5",
-      }}
-    ></div>
-  );
+  return <CategoryWrapper></CategoryWrapper>;
 }
