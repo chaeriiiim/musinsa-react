7fa5f7c6e11b3cf537e7103acfff80ce3893a3ca
김채림 <woqja0905@naver.com>
2025-04-24 15:20:16 +0900

    update : title section main 페이지에 추가

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,11 +1,13 @@
 import CategoryButton from "./CategoryButton";
 import MainSlider from "./MainSlider";
+import TitleSection from "./TitleSection";
 
 export default function Main() {
   return (
     <>
       <MainSlider />
       <CategoryButton />
+      <TitleSection />
     </>
   );
 }
