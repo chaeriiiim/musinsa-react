4bbba9210d798cef9ff42badd8d37651b526dcb8
김채림 <woqja0905@naver.com>
2025-04-24 15:19:37 +0900

    update: title section 코드

---
+++ src/pages/Main/TitleSection.tsx
@@ -0,0 +1,12 @@
+export default function TitleSection() {
+  return (
+    <div
+      style={{
+        width: "100%",
+        height: "62px",
+        padding: "12px 16px",
+        backgroundColor: "white",
+      }}
+    ></div>
+  );
+}
