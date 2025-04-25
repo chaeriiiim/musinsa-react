669d840cb5ea794a3ca1c0368aff066c8bb9c104
김채림 <woqja0905@naver.com>
2025-04-25 21:32:29 +0900

    add : img 추가

--- src/pages/Main/ProductSlider.tsx
+++ src/pages/Main/ProductSlider.tsx
@@ -1,35 +1,73 @@
+import { Link } from "react-router-dom";
+import styled from "styled-components";
 import Product_1 from "../../assets/product_1.webp";
+
+const Root = styled.div`
+  height: 402px;
+  width: 100%;
+  background-color: #f5f5f5;
+`;
+
+const productList = [
+  {
+    path: "/",
+    img: Product_1,
+    brand: "엄브로",
+    productName: "클래식 윔업 바람막이 자켓 아이보리",
+    per: "60%",
+    price: "59,000원",
+  },
+];
 
 export default function ProductSlider() {
   return (
-    <div style={{ height: "402px", width: "100%", backgroundColor: "#F5F5F5" }}>
-      <div
-        style={{ height: "312px", width: "260px", backgroundColor: "#E2E2E2" }}
-      >
-        <img src={Product_1} style={{ width: "260px", height: "312px" }} />
-      </div>
-
-      <div
-        style={{
-          height: "90px",
-          width: "260px",
-          lineHeight: "7px",
-          padding: "8px",
-        }}
-      >
-        <p style={{ fontSize: "11px", fontWeight: "bold" }}>엄브로</p>
-        <p style={{ fontSize: "13px" }}>클래식 윔업 바람막이 자켓 아이보리</p>
-        <div style={{}}>
-          <span style={{ fontSize: "13px", color: "red", fontWeight: "bold" }}>
-            60%
-          </span>
-          <span
-            style={{ fontSize: "13px", marginLeft: "5px", fontWeight: "bold" }}
+    <Root>
+      {productList.map((product, index) => (
+        <Link to={product.path} key={index}>
+          <div
+            style={{
+              height: "312px",
+              width: "260px",
+              backgroundColor: "#E2E2E2",
+            }}
           >
-            59,000원
-          </span>
-        </div>
-      </div>
-    </div>
+            <img
+              src={product.img}
+              style={{ width: "260px", height: "312px" }}
+            />
+          </div>
+
+          <div
+            style={{
+              height: "90px",
+              width: "260px",
+              lineHeight: "7px",
+              padding: "8px",
+            }}
+          >
+            <p style={{ fontSize: "11px", fontWeight: "bold" }}>
+              {product.brand}
+            </p>
+            <p style={{ fontSize: "13px" }}>{product.productName}</p>
+            <div style={{}}>
+              <span
+                style={{ fontSize: "13px", color: "red", fontWeight: "bold" }}
+              >
+                {product.per}
+              </span>
+              <span
+                style={{
+                  fontSize: "13px",
+                  marginLeft: "5px",
+                  fontWeight: "bold",
+                }}
+              >
+                {product.price}
+              </span>
+            </div>
+          </div>
+        </Link>
+      ))}
+    </Root>
   );
 }
