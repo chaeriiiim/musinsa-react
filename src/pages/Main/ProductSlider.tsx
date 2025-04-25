f5bec5b222210b3877cbd31303e44dd7ac7a92c3
김채림 <woqja0905@naver.com>
2025-04-25 21:46:33 +0900

    update : productSlider 수정

--- src/pages/Main/ProductSlider.tsx
+++ src/pages/Main/ProductSlider.tsx
@@ -1,17 +1,54 @@
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 import Product_1 from "../../assets/product_1.webp";
+import Product_2 from "../../assets/product_2.webp";
 
 const Root = styled.div`
   height: 402px;
   width: 100%;
   background-color: #f5f5f5;
+  display: flex;
+`;
+
+const ImgWrapper = styled.div`
+  height: 312px;
+  width: 260px;
+  background-color: #e2e2e2;
+
+  & img {
+    width: 260px;
+    height: 312px;
+  }
+`;
+
+const ProductTitleWrapper = styled.div`
+  height: 90px;
+  width: 260px;
+  line-height: 7px;
+  padding: 8px;
+`;
+
+const ProductBrand = styled.p`
+  font-size: 11px;
+  font-weight: bold;
+`;
+
+const ProductName = styled.p`
+  font-size: 13px;
 `;
 
 const productList = [
   {
     path: "/",
     img: Product_1,
+    brand: "엄브로",
+    productName: "클래식 윔업 바람막이 자켓 아이보리",
+    per: "60%",
+    price: "59,000원",
+  },
+  {
+    path: "/",
+    img: Product_2,
     brand: "엄브로",
     productName: "클래식 윔업 바람막이 자켓 아이보리",
     per: "60%",
@@ -24,32 +61,14 @@ export default function ProductSlider() {
     <Root>
       {productList.map((product, index) => (
         <Link to={product.path} key={index}>
-          <div
-            style={{
-              height: "312px",
-              width: "260px",
-              backgroundColor: "#E2E2E2",
-            }}
-          >
-            <img
-              src={product.img}
-              style={{ width: "260px", height: "312px" }}
-            />
-          </div>
+          <ImgWrapper>
+            <img src={product.img} alt="제품 사진" />
+          </ImgWrapper>
 
-          <div
-            style={{
-              height: "90px",
-              width: "260px",
-              lineHeight: "7px",
-              padding: "8px",
-            }}
-          >
-            <p style={{ fontSize: "11px", fontWeight: "bold" }}>
-              {product.brand}
-            </p>
-            <p style={{ fontSize: "13px" }}>{product.productName}</p>
-            <div style={{}}>
+          <ProductTitleWrapper>
+            <ProductBrand>{product.brand}</ProductBrand>
+            <ProductName>{product.productName}</ProductName>
+            <div>
               <span
                 style={{ fontSize: "13px", color: "red", fontWeight: "bold" }}
               >
@@ -65,7 +84,7 @@ export default function ProductSlider() {
                 {product.price}
               </span>
             </div>
-          </div>
+          </ProductTitleWrapper>
         </Link>
       ))}
     </Root>
