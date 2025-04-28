86ff29a0219bb6b6553ff5dc2fdd9b9b8cab78c8
김채림 <woqja0905@naver.com>
2025-04-28 20:53:40 +0900

    fix:css 수정

--- src/pages/Main/MainSlider.tsx
+++ src/pages/Main/MainSlider.tsx
@@ -4,17 +4,35 @@ export default function MainSlider() {
   return (
     <div
       style={{
-        position: "relative",
         display: "flex",
         alignItems: "flex-end",
+        width: "480px",
+        height: "480px",
       }}
     >
       <img src={MainSliderImg1} style={{ width: "480px", height: "480px" }} />
-      <span
-        style={{ position: "absolute", color: "white", marginBottom: "50px" }}
-      >
-        여름맞이
-      </span>
+      <div style={{ position: "absolute", height: "120px" }}>
+        <p
+          style={{
+            color: "white",
+            fontSize: "22px",
+            fontWeight: "bold",
+          }}
+        >
+          여름맞이 신상
+          <br />
+          15% 쿠폰 혜택
+        </p>
+        <p
+          style={{
+            color: "white",
+            fontSize: "13px",
+            fontWeight: "bold",
+          }}
+        >
+          무신사 스탠다드
+        </p>
+      </div>
     </div>
   );
 }
