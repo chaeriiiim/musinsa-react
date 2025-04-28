0b28300c8247c65db88d7ed80f3232712ee9a0db
김채림 <woqja0905@naver.com>
2025-04-28 21:05:28 +0900

    update : img 수정

--- src/pages/Main/MainSlider.tsx
+++ src/pages/Main/MainSlider.tsx
@@ -1,38 +1,78 @@
+import { Link } from "react-router-dom";
 import MainSliderImg1 from "../../assets/main_img1.jpg";
+import MainSliderImg2 from "../../assets/main_img2.jpg";
+import MainSliderImg3 from "../../assets/main_img3.jpg";
+
+const MainSliderList = [
+  {
+    src: MainSliderImg1,
+    path: "#",
+    title: "여름맞이 신상",
+    subtitle: "15% 쿠폰 혜택",
+    brand: "무신사 스탠다드",
+  },
+  {
+    src: MainSliderImg2,
+    path: "#",
+    title: "여름맞이 신상",
+    subtitle: "15% 쿠폰 혜택",
+    brand: "무신사 스탠다드",
+  },
+  {
+    src: MainSliderImg3,
+    path: "#",
+    title: "여름맞이 신상",
+    subtitle: "15% 쿠폰 혜택",
+    brand: "무신사 스탠다드",
+  },
+];
 
 export default function MainSlider() {
   return (
-    <div
-      style={{
-        display: "flex",
-        alignItems: "flex-end",
-        width: "480px",
-        height: "480px",
-      }}
-    >
-      <img src={MainSliderImg1} style={{ width: "480px", height: "480px" }} />
-      <div style={{ position: "absolute", height: "120px" }}>
-        <p
+    <div style={{ display: "flex" }}>
+      {MainSliderList.map((item, index) => (
+        <Link
           style={{
-            color: "white",
-            fontSize: "22px",
-            fontWeight: "bold",
+            display: "flex",
+            alignItems: "flex-end",
+            width: "480px",
+            height: "480px",
           }}
+          key={index}
+          to={item.path}
         >
-          여름맞이 신상
-          <br />
-          15% 쿠폰 혜택
-        </p>
-        <p
-          style={{
-            color: "white",
-            fontSize: "13px",
-            fontWeight: "bold",
-          }}
-        >
-          무신사 스탠다드
-        </p>
-      </div>
+          <img src={item.src} style={{ width: "480px", height: "480px" }} />
+          <div
+            style={{
+              position: "absolute",
+              height: "120px",
+              marginLeft: "10px",
+            }}
+          >
+            <p
+              style={{
+                color: "white",
+                fontSize: "22px",
+                fontWeight: "bold",
+                marginBottom: "10px",
+              }}
+            >
+              {item.title}
+              <br />
+              {item.subtitle}
+            </p>
+            <p
+              style={{
+                color: "white",
+                fontSize: "13px",
+                fontWeight: "bold",
+              }}
+            >
+              {item.brand}
+            </p>
+          </div>
+        </Link>
+      ))}
     </div>
   );
 }
