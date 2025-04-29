4ac9172366dcaa49f3baa2561c573f5e0cd3ff37
김채림 <woqja0905@naver.com>
2025-04-29 11:39:08 +0900

    add : img 추가

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -1,3 +1,30 @@
+import styled from "styled-components";
+import ButtonImg from "../../assets/button_img.svg";
+
+const Root = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+const ButtonWrapper = styled.div`
+  height: 36px;
+  width: 600px;
+  background-color: #38b0f4;
+  border-radius: 5px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
 export default function Button() {
-  return;
+  return (
+    <Root>
+      <ButtonWrapper>
+        <img src={ButtonImg} style={{ marginRight: "5px" }}></img>
+        <p style={{ color: "white", fontSize: "14px", fontWeight: "bold" }}>
+          무신사 플레이어에서 더보기
+        </p>
+      </ButtonWrapper>
+    </Root>
+  );
 }
