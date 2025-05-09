4fbf677ce32f44589ea77cdf034a42b56229a8bc
김채림 <woqja0905@naver.com>
2025-05-09 11:36:19 +0900

    update : 라우터 수정

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
 import Footer from "./components/common/Footer";
 import Header from "./components/common/Header";
-import Main from "./pages/Main/Main";
+import Main from "./pages/main/Main";
 import GlobalStyle from "./styles/GlobalStyle";
 import theme from "./styles/theme";
 
